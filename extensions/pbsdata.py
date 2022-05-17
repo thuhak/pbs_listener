940c374945daee8dfcba95b11b8d0df7f6ced3eb
thuhak.zhou <thuhak.zhou@nio.com>
2022-05-17 14:02:13 +0800

    fix email

--- extensions/pbsdata.py
+++ extensions/pbsdata.py
@@ -73,7 +73,7 @@ class JobData(UserDict):
         'job_name': KeyType('Job_Name', str),
         'project': KeyType('project', str),
         'user': KeyType('euser', str),
-        'email': KeyType('email', str),
+        'email': KeyType('Mail_users', str),
         'queue': KeyType('queue', str),
         'priority': KeyType('Priority', int),
         'run_count': KeyType('run_count', int),

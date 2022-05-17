6eae4f5893351a29032cb7b2849283c8e5bb635b
thuhak.zhou <thuhak.zhou@nio.com>
2022-05-17 10:44:17 +0800

    修改department的数据结构，以便更好的分析

--- extensions/user_db.py
+++ extensions/user_db.py
@@ -17,16 +17,16 @@ class UserInfo:
         result = {}
         dep_cypher = f'''
         MATCH (:People {{workerUserId: '{username}'}})-[:WORK_AT]->(d:Department)
-        RETURN d.name AS dep UNION
+        RETURN d.name AS dep, d.tier as tier UNION
         MATCH (:People {{workerUserId: '{username}'}})-[:WORK_AT]->(:Department)-[:SUP_DEP*]->(up:Department)
-        RETURN up.name AS dep'''
+        RETURN up.name AS dep, up.tier as tier'''
         email_cypher = f'''
         MATCH (n:People {{workerUserId: '{username}'}})
         RETURN n.email AS email
         '''
         logger.info(f'getting date of {username}')
         try:
-            deps = [x['dep'] for x in self.graph.run(dep_cypher).data()]
+            deps = {f't{i["tier"]}': i["dep"] for i in self.graph.run(dep_cypher).data()}
             email = self.graph.run(email_cypher).data()[0]['email']
             result = {
                 'email': email,

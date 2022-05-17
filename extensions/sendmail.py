ad7584679aabbc8dfcf731c815adbe0d9122a562
thuhak.zhou <thuhak.zhou@nio.com>
2022-05-17 10:43:56 +0800

    fix sendmail error

--- extensions/sendmail.py
+++ extensions/sendmail.py
@@ -3,6 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 import logging
+from threading import Lock
 
 from jinja2 import Environment, PackageLoader
 from . import JobData
@@ -13,11 +14,21 @@ env = Environment(loader=PackageLoader('extensions', 'mail_templates'))
 
 class Mail:
     def __init__(self, smtp_host: str, smtp_user: str, smtp_pass: str, smtp_port=25):
-        self.server = smtplib.SMTP(smtp_host, smtp_port)
+        self.server = smtplib.SMTP()
+        self.smtp_host = smtp_host
+        self.smtp_port = smtp_port
         self.smtp_user = smtp_user
         self.smtp_pass = smtp_pass
         self.template = env.get_template('job_finish')
         self.export_keys = ['job_name', 'job_id', 'total_hours', 'wait_hours', 'run_hours', 'app', 'job_file', 'cores', 'memory']
+        self.lock = Lock()
+
+    def _is_connected(self):
+        try:
+            status = self.server.noop()[0]
+        except:
+            status = -1
+        return True if status == 250 else False
 
     def __call__(self, jobdata: JobData):
         try:
@@ -32,7 +43,10 @@ class Mail:
             content = self.template.render(**email_data)
             msg.attach(MIMEText(content, 'plain', 'utf-8'))
             logger.info(f'sending mail to {email}')
-            self.server.login(self.smtp_user, self.smtp_pass)
-            self.server.sendmail(self.smtp_user, [email], msg.as_string())
+            with self.lock:
+                if not self._is_connected():
+                    self.server.connect(self.smtp_host, self.smtp_port)
+                    self.server.login(self.smtp_user, self.smtp_pass)
+                self.server.sendmail(self.smtp_user, [email], msg.as_string())
         except Exception as e:
             logger.error(f'send mail failed, reason {str(e)}')
